19e1ca542dae876dfb7ef6f1f797efe1f4ce6c00
Revathi-j <revathi.blisslogix@gmail.com>
2016-04-08 18:58:38 +0530

    e

--- ApplicationtestTests/ApplicationtestTests.swift
+++ ApplicationtestTests/ApplicationtestTests.swift
@@ -28,7 +28,7 @@ class ApplicationtestTests: XCTestCase {
     
     func testPerformanceExample() {
         
-       let vc = ViewController()
+       
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
